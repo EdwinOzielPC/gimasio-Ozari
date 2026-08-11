3d38eac57f1ed3b3e13734588b18451a8cfb1ca1
CC_Laboratorio <CC_Laboratorio@CC02W02.utleon.edu.mx>
2026-08-11 15:20:49 -0600

    commmit exepcion

---
+++ src/pkg_modelo/ReglaNegocioException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg_modelo;
+
+/**
+ *
+ * @author CC_Laboratorio
+ */
+  
+public class ReglaNegocioException extends Exception {
+ 
+    public ReglaNegocioException(String mensaje) {
+        super(mensaje);
+    }
+}
+
