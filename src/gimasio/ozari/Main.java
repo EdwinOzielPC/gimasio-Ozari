de899c1ac40e403b6dc18731e67a3afb6ff3db02
CC_Laboratorio <CC_Laboratorio@CC09W24.utleon.edu.mx>
2026-08-12 10:14:18 -0600

    commit main

--- src/gimasio/ozari/Main.java
+++ src/gimasio/ozari/Main.java
@@ -4,6 +4,8 @@
  */
 package gimasio.ozari;
 
+import pkg_vista.vista;
+
 /**
  *
  * @author CC_Laboratorio
@@ -15,6 +17,14 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+     java.awt.EventQueue.invokeLater(() -> {
+
+            vista ventana = new vista();
+
+            ventana.setTitle("Sistema de Gestión de Gimnasio");
+            ventana.setLocationRelativeTo(null);
+            ventana.setVisible(true);
+        });
     }
     
 }
